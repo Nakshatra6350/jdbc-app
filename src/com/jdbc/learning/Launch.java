a7a87439204853a7479663783ebee71123f04d15
Nakshatra Garg <nakshatra@kugelblitz.xyz>
2026-04-16 15:18:51 +0530

    first commit

---
+++ src/com/jdbc/learning/Launch.java
@@ -0,0 +1,48 @@
+package com.jdbc.learning;
+
+import java.sql.*;
+
+public class Launch {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        try {
+            //load and register driver, during this class loading the static block of this class gets executed automatically
+            Class.forName("com.mysql.cj.jdbc.Driver"); //this means load the given class
+            //DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver()); // another way
+
+            //establish connection
+            String url = "jdbc:mysql://127.0.0.1:3306/jdbclearning";
+            String user = "root";
+            String password = "root";
+            Connection connection = DriverManager.getConnection(url, user, password);
+
+            // create statement
+            Statement statement = connection.createStatement();
+
+            //execute query
+            String sql = "INSERT INTO studentinfo(id, sname, sage, scity) VALUES(2, 'SK', 23, 'KOTA')";
+            int rowAffected = statement.executeUpdate(sql);
+
+            //process the result
+            if(rowAffected == 0){
+                System.out.println("Unable to update data");
+            }else{
+                System.out.println("Data inserted successfully");
+            }
+
+            //close the resources/connection
+            statement.close();
+            connection.close();
+
+        }catch (ClassNotFoundException e) {
+            System.out.println("Class not found to load!" + e.getMessage());
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Connection failed!" + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            System.out.println("Connection estalished and closed");
+        }
+
+
+    }
+}
